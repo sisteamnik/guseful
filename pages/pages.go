5ceeb1df82a87361a218f778b7948fd1d94f09c1
Live session user <sisteamnik@mail.ru>
2014-06-01 19:05:44 +0000

    add Page struct

--- pages/pages.go
+++ pages/pages.go
@@ -1,2 +1,12 @@
 package pages
 
+type Page struct {
+  Id           int64
+  Title        string
+  Description  string
+  Body         string
+  Created      int64
+  Modified     int64
+  PhotoId      int64
+  Author       string
+}
