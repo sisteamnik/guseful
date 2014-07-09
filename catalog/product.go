76f3bf767c4a676419aa343da7680fc27de42e01
sisteamnik <sisteamnik@mail.ru>
2014-07-09 20:00:27 +0400

    update product structure

--- catalog/product.go
+++ catalog/product.go
@@ -1,50 +1,27 @@
 package catalog
 
 import (
-	"fmt"
 	"time"
 )
 
 type Product struct {
 	Id           int64
+	Price        float64
 	VendorId     int64
 	CollectionId int64
 	ModelId      int64
-	Title        string
-	Description  string
-	Body         string
-	Store        bool
+	Status       int64
+	Quantity     int64
 	CategoryId   int64
 	Delivery     bool
-	ImgId        int64
+	ImgUrl       string
 	Created      int64
 	Modified     int64
 	Published    bool
+	Viewed       int64
 	Properties   []ProductProperty
 }
 
 func (p *Product) UpdateModifiedDate() {
 	p.Modified = time.Now().UnixNano()
-}
-
-func BuildProperties(props []Property) (r []ProductProperty){
-  for i:= range props {
-  	if props[i].ParentId == 0 {
-  	  l := ProductProperty{
-        Id: props[i].Id,
-        Name: props[i].Name,
-        Position: props[i].Position,
-  	  }
-  	  for j := range props {
-  	  	if props[j].ParentId == props[i].Id {
-  	  		if props[j].Shortable {
-				props[j].Short = fmt.Sprintf(props[j].ShortTemplate, props[j].Value)
-			}
-  	  	  l.Childs = append(l.Childs, props[j])
-  	  	}
-  	  }
-  	  r = append(r, l)
-  	}
-  }
-  return
 }
--- catalog/property.go
+++ catalog/property.go
@@ -1,42 +1,18 @@
 package catalog
 
-type PropertyName struct {
-	Id            int64
-	Name          string
-	IsShortable   bool
-	ShortTemplate string
-	ParentId      int64
-	Position      int64
-}
-
-type PropertyValue struct {
-	Id       int64
-	Value    string
-	ParentId int64
-}
-
-type PropertiesBind struct {
-	BindId          int64
+type Property struct {
+	Id              int64
+	ProductId       int64
 	PropertyId      int64
 	PropertyValueId int64
 }
 
-type Property struct {
-	Id        int64
-	Name      string
-	Value     string
-	Shortable bool
-	Short     string
-	ShortTemplate string
-
-	Position  int64
-
-	ParentId  int64
-}
-
-type ProductProperty struct {
+type PropertyName struct {
 	Id   int64
 	Name string
-	Position int64
-	Childs []Property
+}
+
+type PropertyValue struct {
+	Id    int64
+	Value string
 }
