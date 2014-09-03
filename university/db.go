f555167bdfcc0c16ab4365d369f3fdb7713fe3f3
sisteamnik <sisteamnik@mail.ru>
2014-09-04 00:31:10 +0400

    create univercity.schedule and work logic

--- university/db.go
+++ university/db.go
@@ -2,6 +2,7 @@ package university
 
 import (
 	"github.com/coopernurse/gorp"
+	"sort"
 )
 
 func NewUniversity(db *gorp.DbMap) *University {
@@ -17,6 +18,7 @@ func NewUniversity(db *gorp.DbMap) *University {
 func (u *University) AddTables() error {
 	u.db.AddTable(ScheduleItem{}).SetKeys(true, "Id")
 	u.db.AddTable(PeriodType{}).SetKeys(true, "Id")
+	u.db.AddTable(Subject{}).SetKeys(true, "Id")
 	u.db.AddTable(Audithory{}).SetKeys(true, "Id")
 	u.db.AddTable(Corps{}).SetKeys(true, "Id")
 	u.db.AddTable(Group{}).SetKeys(true, "Id")
@@ -30,8 +32,148 @@ func (u *University) CreateScheduleItem(s *ScheduleItem) error {
 	return u.db.Insert(s)
 }
 
-func (u *University) GetScheduleForGroup(groupId int64) ([]ScheduleItem, error) {
-	si := []ScheduleItem{}
-	_, err := u.db.Select(&si, "select * from ScheduleItem where Group = ?", groupId)
-	return si, err
+func (u *University) GetScheduleForGroup(groupId int64) (Schedule, error) {
+	si := []ScheduleItemView{}
+	_, err := u.db.Select(&si, "select ScheduleItem.Id, PeriodType, Guru, Subject, "+
+		"Audithory, Corps, "+
+		" Start,Duration ,WeekDay, `Group`,TrainingType, Attendance,Billing, "+
+		"PeriodType.Title as PeriodTypeName,'Препод' as GuruName,"+
+		" `Subject`.`Title` as SubjectName,"+
+		" Audithory.Title as AudithoryName,Corps.Title as CorpsName,"+
+		" '' as StartName,'' as DurationName, "+
+		"'' as WeekDayName, `Group`.Title GroupName,"+
+		"TrainingType.Title TrainingTypeName,"+
+		"Attendance.Title as AttendanceName, Billing.Title as BillingName "+
+		"from ScheduleItem, PeriodType, Audithory, Corps, `Group`, TrainingType,"+
+		" Attendance, Billing, Subject  where PeriodType.Id"+
+		" = ScheduleItem.PeriodType and Audithory.Id = Audithory and Corps.Id"+
+		" = Corps and `Group`.Id = `Group` and TrainingType.Id = TrainingType"+
+		" and Subject.Id = Subject and ScheduleItem.`Group` in (select Id from"+
+		" `Group` where Parent = ? or Id = ?)", groupId, groupId)
+	for i := range si {
+		si[i].WeekDayName = weekDay(si[i].WeekDay)
+	}
+	result := Schedule(si)
+	return result, err
+}
+
+func (s Schedule) MapWeekDay() map[int64]map[int64]map[int64][]ScheduleItemView {
+	var result = map[int64]map[int64]map[int64][]ScheduleItemView{}
+	var i, j, k int64
+	for i = 1; i <= 7; i++ {
+		result[i] = map[int64]map[int64][]ScheduleItemView{}
+		startes := s.Startes()
+		for j = 0; j < int64(len(startes)); j++ {
+			result[i][startes[j]] = map[int64][]ScheduleItemView{}
+			periodTypes := s.PeriodTypes()
+			for k = 0; k < int64(len(periodTypes)); k++ {
+				for _, v := range s {
+					if v.WeekDay == i && startes[j] == v.Start && v.PeriodType == periodTypes[k] {
+						result[i][startes[j]][periodTypes[k]] = append(result[i][startes[j]][periodTypes[k]], v)
+					}
+				}
+			}
+		}
+	}
+	return result
+}
+
+func (s Schedule) SortByTime() Schedule {
+	sort.Sort(ByTime(s))
+	return s
+}
+
+func (u *University) CreatePeriodType(name string) (PeriodType, error) {
+	pt := PeriodType{}
+	pt.Title = name
+	err := u.db.Insert(&pt)
+	return pt, err
+}
+
+func (u *University) GetPeriodTypes() []PeriodType {
+	var res []PeriodType
+	u.db.Select(&res, "select * from PeriodType")
+	return res
+}
+
+func (u *University) GetAudithories() []Audithory {
+	var res []Audithory
+	u.db.Select(&res, "select * from Audithory")
+	return res
+}
+
+func (u *University) GetGroups() []Group {
+	var res []Group
+	u.db.Select(&res, "select * from `Group`")
+	return res
+}
+
+func (u *University) GetCorpuses() []Corps {
+	var res []Corps
+	u.db.Select(&res, "select * from Corps")
+	return res
+}
+
+func (u *University) GetTrainingTypes() []TrainingType {
+	var res []TrainingType
+	u.db.Select(&res, "select * from TrainingType")
+	return res
+}
+
+func (u *University) GetAttendances() []Attendance {
+	var res []Attendance
+	u.db.Select(&res, "select * from Attendance")
+	return res
+}
+
+func (u *University) GetBillings() []Billing {
+	var res []Billing
+	u.db.Select(&res, "select * from Billing")
+	return res
+}
+
+func (u *University) GetSubjects() []Subject {
+	var res []Subject
+	u.db.Select(&res, "select * from Subject")
+	return res
+}
+
+func weekDay(id int64) string {
+	var days = map[int64]string{
+		1: "Понедельник",
+		2: "Вторник",
+		3: "Среда",
+		4: "Четверг",
+		5: "Пятница",
+		6: "Суббота",
+		7: "Воскресенье",
+	}
+	return days[id]
+}
+
+func couple(start int64) string {
+	var couples = map[int64]string{
+		510: "8:30-10:00",
+		610: "10:10-11:40",
+		720: "12:00-13:30",
+		820: "13:40-15:10",
+		920: "15:20-16:50",
+		1:   "8:30-10:00",
+		2:   "10:10-11:40",
+		3:   "12:00-13:30",
+		4:   "13:40-15:10",
+		5:   "15:20-16:50",
+	}
+	return couples[start]
+}
+
+func coupleNumber(start int64) int64 {
+	var couples = map[int64]int64{
+		510: 1,
+		610: 2,
+		720: 3,
+		820: 4,
+		920: 5,
+	}
+	return couples[start]
 }
--- university/university.go
+++ university/university.go
@@ -2,6 +2,7 @@ package university
 
 import (
 	"github.com/coopernurse/gorp"
+	"sort"
 )
 
 type (
@@ -9,14 +10,13 @@ type (
 		db *gorp.DbMap
 	}
 
-	Schedule struct {
-		Items []ScheduleItem
-	}
+	Schedule []ScheduleItemView
 
 	ScheduleItem struct {
 		Id           int64
 		PeriodType   int64
 		Guru         int64
+		Subject      int64
 		Audithory    int64
 		Corps        int64
 		Start        int64
@@ -28,12 +28,32 @@ type (
 		Billing      int64
 	}
 
+	ScheduleItemView struct {
+		ScheduleItem
+		PeriodTypeName   string
+		GuruName         string
+		SubjectName      string
+		AudithoryName    string
+		CorpsName        string
+		StartName        string
+		DurationName     string
+		WeekDayName      string
+		GroupName        string
+		TrainingTypeName string
+		AttendanceName   string
+		BillingName      string
+	}
+
 	itemWithTitle struct {
 		Id    int64
 		Title string
 	}
 
 	PeriodType struct {
+		itemWithTitle
+	}
+
+	Subject struct {
 		itemWithTitle
 	}
 
@@ -45,8 +65,13 @@ type (
 		itemWithTitle
 	}
 
+	WeekDay struct {
+		itemWithTitle
+	}
+
 	Group struct {
 		itemWithTitle
+		Parent int64
 	}
 
 	TrainingType struct {
@@ -61,3 +86,64 @@ type (
 		itemWithTitle
 	}
 )
+
+type ByTime []ScheduleItemView
+
+func (a ByTime) Len() int           { return len(a) }
+func (a ByTime) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByTime) Less(i, j int) bool { return a[i].Start < a[j].Start }
+
+type Int64 []int64
+
+func (a Int64) Len() int           { return len(a) }
+func (a Int64) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a Int64) Less(i, j int) bool { return a[i] < a[j] }
+
+func (s Schedule) MinTime() int64 {
+	var min int64
+	for i := range s {
+		if s[i].Start < min {
+			min = s[i].Start
+		}
+		if i == 0 {
+			min = s[i].Start
+		}
+	}
+	return min
+}
+
+func (s Schedule) MaxTime() int64 {
+	var max int64
+	for i := range s {
+		if (s[i].Start + s[i].Duration) > max {
+			max = s[i].Start + s[i].Duration
+		}
+	}
+	return max
+}
+
+func (s Schedule) Startes() []int64 {
+	var startes = map[int64]bool{}
+	var res []int64
+	for _, v := range s {
+		startes[v.Start] = true
+	}
+	for k, _ := range startes {
+		res = append(res, k)
+	}
+	sort.Sort(Int64(res))
+	return res
+}
+
+func (s Schedule) PeriodTypes() []int64 {
+	var periodTypes = map[int64]bool{}
+	var res []int64
+	for _, v := range s {
+		periodTypes[v.PeriodType] = true
+	}
+	for k, _ := range periodTypes {
+		res = append(res, k)
+	}
+	sort.Sort(Int64(res))
+	return res
+}
